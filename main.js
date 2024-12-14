d5d14e60911168b5d8ccce3e151e65c5d1c9b2f3
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2024-12-14 11:54:52 +0100

    Participant draws

--- main.js
+++ main.js
@@ -1,42 +1,67 @@
 function openModal() {
   document.getElementById("modal").classList.remove("hidden");
-};
+}
 function closeModal() {
   document.getElementById("modal").classList.add("hidden");
-};
+}
 
-  function deleteData(email){
-    const indexOfElement = participants.map(p => p.email).indexOf(email);
+function deleteData(email) {
+  const indexOfElement = participants.map((p) => p.email).indexOf(email);
 
-    document.getElementById('table').deleteRow(indexOfElement+1);
-  };
+  document.getElementById("table").deleteRow(indexOfElement + 1);
+}
 
-  let participants = [];
+let participants = [];
 
-  function addParticipant(name, email){
-    participants.push({name: name, email: email});
-    this.createData(participants[participants.length - 1]);
-    this.closeModal();
+function addParticipant(name, email) {
+  participants.push({ name: name, email: email });
+  this.createData(participants[participants.length - 1]);
+  this.closeModal();
+}
+
+function drawParticipants() {
+  const participantsAleatory = participants.sort(() => {
+    return Math.random() - 0.5;
+  });
+  const draws = [];
+
+  for (let i = 0; i < participantsAleatory.length; i++) {
+    let draw = {};
+    if (i === participantsAleatory.length - 1) {
+      draw = {
+        p1: participantsAleatory[i],
+        p2: participantsAleatory[0],
+      };
+    } else {
+      draw = {
+        p1: participantsAleatory[i],
+        p2: participantsAleatory[i + 1],
+      };
+    }
+
+    draws.push(draw);
   }
+
+  return draws;
+}
 
 let table = document.querySelector("table");
 let tblBody = document.querySelector("tbody");
 
-function createData(participant){
-        const row = document.createElement("tr");
-        row.setAttribute('id', participant.email);
-    
-        for (let j = 0; j <= Object.keys(participant).length; j++) {
-            const cell = document.createElement("td");
-      
-            if(j === Object.keys(participant).length) {
-              cell.innerHTML = `<button onclick=deleteData("${participant.email}") value="${participant.email}">Eliminar</button>`;
-            }else{
-              cell.innerHTML = `<td>${participant[Object.keys(participant)[j]]}</td>`;
-            }
-            row.appendChild(cell);
-        }
-    
-        tblBody.appendChild(row);
-}
+function createData(participant) {
+  const row = document.createElement("tr");
+  row.setAttribute("id", participant.email);
 
+  for (let j = 0; j <= Object.keys(participant).length; j++) {
+    const cell = document.createElement("td");
+
+    if (j === Object.keys(participant).length) {
+      cell.innerHTML = `<button onclick=deleteData("${participant.email}") value="${participant.email}">Eliminar</button>`;
+    } else {
+      cell.innerHTML = `<td>${participant[Object.keys(participant)[j]]}</td>`;
+    }
+    row.appendChild(cell);
+  }
+
+  tblBody.appendChild(row);
+}
